a1a74459ba440df469b78fcdfc608c068293e999
saisai <47806328@qq.com>
2016-08-29 20:41:52 +0800

    PullLoadListView,下拉刷新、上拉加载的listView，首页Fragment中的使用PullLoadListView，首页中的Viewpager作为ListView的头部添加进去，首页中的GridView中图片添加

--- app/src/main/java/com/example/saisai/yoho/fragment/ShouyeFragment.java
+++ app/src/main/java/com/example/saisai/yoho/fragment/ShouyeFragment.java
@@ -1,13 +1,26 @@
 package com.example.saisai.yoho.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import com.example.saisai.yoho.R;
+import com.example.saisai.yoho.adapter.BaseSearchLVAdapter;
 import com.example.saisai.yoho.base.BaseFrament;
+import com.example.saisai.yoho.model.HttpModel;
+import com.example.saisai.yoho.util.DimensUtils;
+import com.example.saisai.yoho.util.HttpUtils;
+import com.example.saisai.yoho.view.MyBanner;
+import com.example.saisai.yoho.view.PullLoadListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Created by saisai on 2016/8/23.
@@ -15,17 +28,85 @@ import com.example.saisai.yoho.base.BaseFrament;
 public class ShouyeFragment extends BaseFrament {
 
 
-    @Override
-    public View initView(LayoutInflater inflater, ViewGroup container) {
+    private PullLoadListView pullLoadListView;
+    private BaseAdapter adapter;
 
-        TextView tv=new TextView(activity);
-        tv.setText(getClass().getSimpleName());
-        tv.setTextSize(30);
-        return tv;
+    @Override
+    public View initView(LayoutInflater inflater, final ViewGroup container) {
+
+       View  inflate =  inflater.inflate(R.layout.fragment_shouye, null);
+
+        MyBanner banner=new MyBanner(activity);
+        AbsListView.LayoutParams bannerParams=new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,DimensUtils.dp2px(200));
+        banner.setLayoutParams(bannerParams);
+        banner.loadData(HttpModel.BANNER,"");
+
+
+        final List<String> list=new ArrayList<>();
+        String[] stringArray = getResources().getStringArray(R.array.navigation_lv_values);
+        list.addAll(Arrays.asList(stringArray));
+        list.addAll(Arrays.asList(stringArray));
+        list.addAll(Arrays.asList(stringArray));
+
+        pullLoadListView= (PullLoadListView) inflate.findViewById(R.id.lv_pull_shouye);
+        pullLoadListView.addHeadView(banner);
+        pullLoadListView.setOnPullOrLoadListener(new PullLoadListView.OnPullOrLoadListener() {
+            @Override
+            public void pull() {
+
+                new android.os.Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        list.add(0,"头部数据");
+                        adapter.notifyDataSetChanged();
+                        pullLoadListView.pullSuccess();
+                    }
+                },3000);
+
+            }
+
+            @Override
+            public void load() {
+                new android.os.Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        list.add("尾部数据");
+                        pullLoadListView.setSelection(pullLoadListView.getFirstVisibilePosition());
+                        adapter.notifyDataSetChanged();
+                        pullLoadListView.loadSuccess();
+                    }
+                },3000);
+            }
+        });
+
+        adapter = new BaseSearchLVAdapter<String>(list,activity) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                TextView tv=new TextView(getContext());
+                tv.setText(list.get(position));
+                tv.setHeight(DimensUtils.dp2px(30));
+                tv.setGravity(Gravity.CENTER);
+                return tv;
+            }
+        };
+        pullLoadListView.setAdapter(adapter);
+
+        return inflate;
     }
 
     @Override
     public void initData() {
+        new HttpUtils().loadData(HttpModel.HOMEPAGER,"").setOnLoadDataListener(new HttpUtils.OnLoadDataListener() {
+            @Override
+            public void loadSuccess(String content) {
 
+            }
+
+            @Override
+            public void loadFailed(String msg) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/saisai/yoho/view/PullLoadListView.java
@@ -0,0 +1,223 @@
+package com.example.saisai.yoho.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.example.saisai.yoho.R;
+import com.example.saisai.yoho.util.DimensUtils;
+
+import java.util.List;
+
+/**
+ * Created by saisai on 2016/8/29.
+ */
+public class PullLoadListView extends RelativeLayout {
+
+
+    private ImageView headView;
+    private LayoutParams headPramas;
+    private ImageView footView;
+    private LayoutParams footParams;
+    private ListView lv;
+    private LayoutParams lvParams;
+    private int headHeight;
+    private int footHeight;
+    private float y;
+
+    public PullLoadListView(Context context) {
+        this(context,null);
+    }
+
+    public PullLoadListView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public PullLoadListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init();
+    }
+
+    private void init() {
+
+
+        headView =new ImageView(getContext());
+        headView.setImageResource(R.drawable.icon_loaing_frame_1);
+        headPramas = new LayoutParams(LayoutParams.MATCH_PARENT, DimensUtils.dp2px(20));
+        headPramas.addRule(CENTER_HORIZONTAL);
+        headView.measure(0,0);
+        headHeight = headView.getMeasuredHeight();
+        headPramas.topMargin=-headHeight;
+        headView.setLayoutParams(headPramas);
+        headView.setId(R.id.pull_load_head);
+
+        footView = new ImageView(getContext());
+        footView.setImageResource(R.drawable.icon_loaing_frame_1);
+        footParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        footParams.addRule(ALIGN_PARENT_BOTTOM);
+        footView.measure(0,0);
+        footHeight = footView.getMeasuredHeight();
+        footParams.bottomMargin=-footHeight;
+        footView.setLayoutParams(footParams);
+        footView.setId(R.id.pull_load_foot);
+
+
+        lv = new ListView(getContext());
+        lvParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        lvParams.addRule(BELOW,R.id.pull_load_head);
+        lvParams.addRule(ABOVE,R.id.pull_load_foot);
+        lv.setLayoutParams(lvParams);
+
+        addView(headView);
+        addView(footView);
+        addView(lv);
+    }
+
+    public void setAdapter(BaseAdapter adapter){
+        lv.setAdapter(adapter);
+    }
+
+    public void addHeadView(View head){
+        lv.addHeaderView(head);
+    }
+    public void addFootView(View foot){
+        lv.addFooterView(foot);
+    }
+
+    public int getFirstVisibilePosition(){
+        return lv.getFirstVisiblePosition();
+    }
+    public void setSelection(int position){
+        lv.setSelection(position);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                y=ev.getY();
+                if(isPull||isLoad){
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float moveY = ev.getY() - y;
+                if(moveY>0&&lv.getFirstVisiblePosition()==0){
+                    View childAt = lv.getChildAt(0);
+                    if(childAt.getTop()>=0){
+                        return true;
+                    }
+                }
+                Log.e("tag",lv.getLastVisiblePosition()+"===="+(lv.getAdapter().getCount()-1));
+                if(moveY<0&&lv.getLastVisiblePosition()==lv.getAdapter().getCount()-1){
+                    int i = lv.getCount() - lv.getFirstVisiblePosition();
+                    View childAt = lv.getChildAt(i - 1);
+                    if(childAt.getBottom()<=lv.getMeasuredHeight()){
+                        Log.e("tag","true");
+                        return true;
+                    }
+                }
+
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_MOVE:
+
+                if(isPull||isLoad){
+                    break;
+                }
+                float moveY = event.getY() - y;
+                if(moveY>0&&lv.getFirstVisiblePosition()==0){
+                    headPramas.topMargin= (int) (-headHeight+moveY);
+                    headView.setLayoutParams(headPramas);
+                }
+                if(moveY<0&&lv.getLastVisiblePosition()==lv.getAdapter().getCount()-1){
+                    footParams.bottomMargin= (int) (-footHeight-moveY);
+                    footView.setLayoutParams(footParams);
+                    lvParams.topMargin= (int) moveY;
+                    lv.setLayoutParams(lvParams);
+                }
+
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                if(headPramas.topMargin>=0){
+                    isPull=true;
+                    headPramas.topMargin=0;
+                    headView.setLayoutParams(headPramas);
+                    headView.setImageResource(R.drawable.load);
+                    if(onPullOrLoadListener!=null){
+                        onPullOrLoadListener.pull();
+                    }
+                }else {
+                    headPramas.topMargin=-headHeight;
+                    headView.setLayoutParams(headPramas);
+                }
+
+                if(footParams.bottomMargin>=0){
+                    isLoad=true;
+                    footParams.bottomMargin=0;
+                    footView.setLayoutParams(footParams);
+                    footView.setImageResource(R.drawable.load);
+                    if(onPullOrLoadListener!=null){
+                        onPullOrLoadListener.load();
+                    }
+                }else {
+                    footParams.bottomMargin=-footHeight;
+                    footView.setLayoutParams(footParams);
+                }
+                break;
+        }
+
+        return true;
+    }
+
+
+    boolean isPull;
+    boolean isLoad;
+
+    public void pullSuccess(){
+        isPull=false;
+        headView.setImageResource(R.drawable.icon_loaing_frame_1);
+        headPramas.topMargin=-headHeight;
+        headView.setLayoutParams(headPramas);
+    }
+
+    public void loadSuccess(){
+        isLoad=false;
+        footView.setImageResource(R.drawable.icon_loaing_frame_1);
+        footParams.bottomMargin=-footHeight;
+        footView.setLayoutParams(footParams);
+        lvParams.topMargin=0;
+        lv.setLayoutParams(lvParams);
+    }
+
+    public interface OnPullOrLoadListener{
+        void pull();
+        void load();
+    }
+
+    private OnPullOrLoadListener onPullOrLoadListener;
+    public void setOnPullOrLoadListener(OnPullOrLoadListener onPullOrLoadListener){
+        this.onPullOrLoadListener=onPullOrLoadListener;
+    }
+}
